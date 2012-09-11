a9e5f79d74cc4bf78a679eb1e5f87f72360571fc
John Wiegley <johnw@newartisans.com>
2012-09-11 02:08:49 -0500

    Everything's working, all space leaks gone

--- Main.hs
+++ Main.hs
@@ -4,8 +4,9 @@
 
 module Main where
 
-import           Control.Applicative
 import           Control.Concurrent.ParallelIO
+import           Control.DeepSeq
+import           Control.Exception
 import           Control.Lens
 import           Control.Monad hiding (sequence)
 import           Data.Function
@@ -36,47 +37,47 @@ data SizesOpts = SizesOpts { jobs      :: Int
                            , byCount   :: Bool
                            , smalls    :: Bool
                            -- , dirsOnly  :: Bool
-                           , recurse   :: Bool
+                           , depth     :: Int
                            , dirs      :: [String] }
                deriving (Data, Typeable, Show, Eq)
 
 sizesOpts :: SizesOpts
 sizesOpts = SizesOpts
     { jobs     = def &= name "j" &= typ "INT"
-                     &= help "Run INT parallel jobs at once (def: # of procs)"
+                     &= help "Run INT concurrent finds at once (default: 2)"
     , byCount  = def &= typ "BOOL"
-                     &= help "Sort output from smallest count to largest"
+                     &= help "Sort output by count (default: by size)"
     , smalls   = def &= typ "BOOL"
                      &= help "Also show small (<1M && <100 files) entries"
     -- , dirsOnly = def &= typ "BOOL"
     --                  &= help "Show directories only"
-    , recurse  = def &= typ "BOOL"
-                     &= help "Report all entries recursively"
-    , dirs     = def &= args &= typDir } &=
+    , depth    = def &= typ "INT"
+                     &= help "Report all entries to a depth of INT (default: 0)"
+    , dirs     = def &= args &= typ "DIRS..." } &=
     summary sizesSummary &=
     program "sizes" &=
     help "Calculate amount of disk used by the given directories"
 
 data EntryInfo = EntryInfo { _entryPath  :: !FilePath
                            , _entryCount :: !Int
-                           , _entrySize  :: !Int }
+                           , _entrySize  :: !Int
+                           , _entryIsDir :: !Bool }
              deriving Show
 
-newEntry :: FilePath -> EntryInfo
-newEntry p = EntryInfo p 0 0
+instance NFData EntryInfo where
+  rnf a = a `seq` ()
+
+newEntry :: FilePath -> Bool -> EntryInfo
+newEntry p isDir = EntryInfo p 0 0 isDir
 
 makeLenses ''EntryInfo
 
 main :: IO ()
 main = do
   mainArgs <- getArgs
-  opts     <- withArgs (if L.null mainArgs then ["--help"] else mainArgs)
+  opts     <- withArgs (if L.null mainArgs then ["."] else mainArgs)
                        (cmdArgs sizesOpts)
-
-  -- Set the default concurrency to equal the number of processors available
-  caps <- GHC.Conc.getNumProcessors
-  _    <- GHC.Conc.setNumCapabilities $ case jobs opts of 0 -> caps; x -> x
-
+  _        <- GHC.Conc.setNumCapabilities $ case jobs opts of 0 -> 2; x -> x
   runSizes opts
 
 runSizes :: SizesOpts -> IO ()
@@ -89,18 +90,16 @@ parMap f xs = parallel (map f xs) >> return ()
 
 reportSizes :: SizesOpts -> [FilePath] -> IO ()
 reportSizes opts xs = do
-  entryInfos <- parallel $ map (gatherSizes (recurse opts)) xs
-
+  entryInfos <- parallel $ map (gatherSizesW (depth opts) 0) xs
   let infos  = map fst entryInfos ++ mconcat (map snd entryInfos)
       sorted = L.sortBy ((compare `on`) $ if byCount opts
                                           then (^. entryCount)
                                           else (^. entrySize)) infos
   forM_ sorted $ \entry ->
-    when (smalls opts || entry^.entrySize >= 10 * 1024^2) $
-      reportEntry entry
-
-toTextIgnore :: FilePath -> Text
-toTextIgnore p = case toText p of Left _ -> ""; Right x -> x
+    when ( smalls opts
+         || entry^.entrySize >= 10 * 1024^2
+         || entry^.entryCount > 100)
+         $ reportEntry entry
 
 humanReadable :: Int -> String
 humanReadable x
@@ -114,37 +113,49 @@ humanReadable x
 
 reportEntry :: EntryInfo -> IO ()
 reportEntry entry =
-  printf (unpack "%10s %10d %s\n")
-         (humanReadable (entry^.entrySize))
-         (entry^.entryCount)
-         (unpack (toTextIgnore (entry^.entryPath)))
+  let path = unpack . toTextIgnore $ entry^.entryPath
+  in printf (unpack "%10s %10d %s%s\n")
+            (humanReadable (entry^.entrySize)) (entry^.entryCount) path
+            (unpack $ if entry^.entryIsDir && L.last path /= '/'
+                      then "/" else "")
 
-gatherSizes :: Bool -> FilePath -> IO (EntryInfo, [EntryInfo])
-gatherSizes aggregate path = do
-  status <- getSymbolicLinkStatus (unpack $ toTextIgnore path)
-  if isSymbolicLink status
-    then return (newEntry path, [])
+toTextIgnore :: FilePath -> Text
+toTextIgnore p = case toText p of Left _ -> ""; Right x -> x
+
+gatherSizesW :: Int -> Int -> FilePath -> IO (EntryInfo, [EntryInfo])
+gatherSizesW m d p =
+  catch (gatherSizes m d p)
+        (\e -> do
+            putStrLn $ show (e :: IOException)
+            return (newEntry p False, []))
+
+gatherSizes :: Int -> Int -> FilePath -> IO (EntryInfo, [EntryInfo])
+gatherSizes maxDepth curDepth path = do
+  let path' = unpack $ toTextIgnore path
+  status <- if curDepth == 0
+            then getFileStatus path'
+            else getSymbolicLinkStatus path'
+  if isDirectory status
+    then do
+      files   <- listDirectory path
+      entries <- let func = gatherSizesW maxDepth (curDepth + 1) . collapse
+                 in if curDepth == 0
+                    then parallel $ map func files
+                    else mapM func files
+      let firsts = map fst entries
+      return $!! ( L.foldl' (\current entry ->
+                           entryCount +~ entry^.entryCount $
+                           entrySize  +~ entry^.entrySize  $ current)
+                         (newEntry path True)
+                         firsts
+                 , if curDepth < maxDepth
+                   then firsts ++ L.concat (map snd entries)
+                   else [] )
     else
-    if isDirectory status
-      then do
-        files   <- listDirectory path
-        entries <- parallel $
-                   map (gatherSizes aggregate . collapse) files
-
-        let firsts = map fst entries
-        return ( L.foldl' (\current entry ->
-                            entryCount +~ entry^.entryCount $
-                            entrySize  +~ entry^.entrySize  $ current)
-                          (newEntry $ fromText $ toTextIgnore path <> "/")
-                          firsts
-               , if aggregate
-                 then firsts ++ L.concat (map snd entries)
-                 else firsts )
-
-      else
-      if isRegularFile status
-        then let size = fromIntegral (fileSize status) in
-             return (entryCount .~ 1 $ entrySize .~ size $ newEntry path, [])
-        else return (newEntry path, [])
+    if isRegularFile status
+      then return ( entryCount .~ 1 $
+                    entrySize .~ fromIntegral (fileSize status) $
+                    newEntry path False, [] )
+      else return (newEntry path False, [])
 
 -- Main.hs (sizes) ends here
